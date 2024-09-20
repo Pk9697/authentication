5881db74753c8fc7594af9e0f2a5b0b6fb9aa3cc
Prashant Kumar <prashant.aman9@gmail.com>
2024-09-20 16:40:16 +0530

    Crud operations using fs

--- index.js
+++ index.js
@@ -1,17 +1,122 @@
-const dotenv=require('dotenv')
-const express = require('express')
+const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const http = require('http')
+const fs = require('fs')
 
 dotenv.config()
-const app = express()
+const server = http.createServer()
 const PORT = process.env.PORT || 8000
-const DB_NAME= 'session-based-authentication'
+const DB_NAME = 'session-based-authentication'
+
+server.on('request', (req, res) => {
+	console.log(req.url, req.method)
+	if (req.method === 'GET' && req.url === '/file') {
+		fs.readFile('./file.txt', (err, data) => {
+			if (err) {
+				console.error(err)
+				return res.end(err)
+			}
+			// console.log(data)
+			return res.end(data)
+		})
+	} else if (req.method === 'GET' && req.url === '/form') {
+		fs.readFile('./form.html', (err, data) => {
+			if (err) {
+				console.error(err)
+				return res.end(err)
+			}
+			// console.log(data)
+			return res.end(data)
+		})
+	} else if (req.method === 'GET' && req.url === '/write') {
+		const data = 'overwritten file'
+		fs.writeFile('./file.txt', data, (err) => {
+			if (err) {
+				console.error(err)
+				return res.end(err)
+			}
+			return res.end(data)
+		})
+	} else if (req.method === 'GET' && req.url === '/append') {
+		const data = 'append some data'
+		fs.appendFile('./file.txt', data, (err) => {
+			if (err) {
+				console.error(err)
+				return res.end(err)
+			}
+			return res.end(data)
+		})
+	} else if (req.method === 'GET' && req.url === '/rename') {
+		// newFile is created then copied from file.txt and file.txt is deleted
+		fs.rename('./file.txt', './newFile.txt', (err) => {
+			if (err) {
+				console.error(err)
+				return res.end(err)
+			}
+			return res.end('rename complete')
+		})
+	} else if (req.method === 'GET' && req.url === '/delete') {
+		if(!fs.existsSync('./file.txt')) {
+			return res.end('File does not exist')
+		}
+		fs.unlink('./file.txt', (err) => {
+			if (err) {
+				console.error(err)
+				return res.end(err)
+			}
+			return res.end('delete complete')
+		})
+	} else {
+		return res.end('Invalid api')
+	}
+
+	// switch (req.method) {
+	// 	case 'GET': {
+	// 		switch (req.url) {
+	// 			case '/': {
+	// 				return res.end('Login get')
+	// 			}
+	// 			case '/form': {
+	// 				return res.end('./form.html')
+	// 			}
+	// 			case '/file': {
+	// 				console.log('here')
+	// 				fs.readFile("./file.txt", (err, data) => {
+	// 					if (err) {
+	// 						console.error("Error",err)
+	// 						return res.end(err)
+	// 					}
+	// 					console.log(data)
+	// 					return res.end(data)
+	// 				})
+	// 				break;
+	// 			}
+	// 				default: {
+	// 					return res.end('Invalid Api 404')
+	// 				}
+	// 		}
+	// 	}
+	// 	case 'POST': {
+	// 		switch (req.url) {
+	// 			case '/': {
+	// 				return res.end('Login post')
+	// 			}
+	// 			default: {
+	// 				return res.end('Invalid api')
+	// 			}
+	// 		}
+	// 	}
+	// 	default: {
+	// 		return res.end('Invalid status method')
+	// 	}
+	// }
+})
 
 mongoose
 	.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
 	.then(() => {
 		console.log('MongoDb connected')
-		app.listen(PORT, (err) => {
+		server.listen(PORT, (err) => {
 			if (err) {
 				console.error(err)
 				process.exit(1)
